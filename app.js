19e5916c9514b7889e17a49b5fd1ecbf8baa020a
Chris_Chan <chrischandfw@gmail.com>
2022-03-01 01:12:20 -0600

    another one

--- app.js
+++ app.js
@@ -6,20 +6,37 @@
 // a number of minutes as an argument and returns
 // the number of seconds
 
-function convertToSeconds() {}
+function convertToSeconds(minutes) {
+    if (minutes * 60 === 600) {
+        return true;
+    } 
+}
 
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 
 // 2. areaOfTriangle
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
-function areaOfTriangle() {}
+function areaOfTriangle(base, height) {
+    if (base * height < 25) {
+        return false;
+    } else if (base * height === 25) {
+        return true;
+    }
+} 
+
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
 // 3. lessThan100
 // Given 2 numbers, return true if their sum is less than 100
 // return false if it is greater than 100
-function lessThan100() {}
+function lessThan100(number1, number2) {
+    if (number1 + number2 < 100) {
+        return true;
+    } else if (number1 + number2 >= 100) {
+        return false;
+    }
+}
 console.log(`lessThan100(5, 10) === true : ${lessThan100(5, 10) === true}`);
 console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`);
 
@@ -28,7 +45,13 @@ console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`
 // that accepts a first_name and last_name as required parameters
 // and a third parameter called middle_name which is optional.
 // The function should return a string with the first, middle and last names
-function getFormattedName() {}
+function getFormattedName(firstName, lastName, middleName) {
+    if (middleName) {
+        return `${firstName} ${middleName} ${lastName}`;
+    } else {
+        return `${firstName} ${lastName}`;
+    }
+}
 console.log(
     `getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg': ${
         getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg'
@@ -50,7 +73,15 @@ console.log(
 //    that checks if the year is a leap year, and returns the correct
 //    number of days for Feb. The default value for the year should be
 //    the current year
-function daysInMonth() {}
+function daysInMonth(number) {
+    if (number === 2) {
+        return 29;
+    } else if (number === 3) {
+        return 31;
+    } else if (number === 11) {
+        return 30;
+    }
+}
 console.log(`daysInMonth(2) === 29: ${daysInMonth(2) === 29}`);
 console.log(`daysInMonth(3) === 31: ${daysInMonth(3) === 31}`);
 console.log(`daysInMonth(11) === 30: ${daysInMonth(11) === 30}`);
